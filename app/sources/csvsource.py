ad71b39842aac0bc5081a270ddf9f55b0b42f374
Edward L Platt <ed@elplatt.com>
2013-09-09 16:25:09 -0400

    Change normalization used in CsvSource.

--- app/sources/csvsource.py
+++ app/sources/csvsource.py
@@ -22,8 +22,6 @@ class CsvSource(Source):
         super(CsvSource, self).__init__(data)
     
     def extract(self):
-        self.data['status'] = TaskStatus.EXTRACTING
-        self.save()
         path = os.path.join(config_upload_folder, self.data['filename'])
         with open(path, 'rU') as csvfile:
             reader = csv.reader(csvfile)
@@ -31,36 +29,24 @@ class CsvSource(Source):
             for row in reader:
                 date = int(time.mktime(datetime.datetime.strptime(row[0], '%Y-%m-%d').timetuple()))
                 db.counts.insert({ 'source_id': self.data['_id'], 'date': date, 'count': float(row[1]) })
-        self.data['status'] = TaskStatus.EXTRACTED
-        self.save()
     
     def transform(self):
-        self.data['status'] = TaskStatus.TRANSFORMING
-        self.save()
+        # Normalize to highest count
         peak = db.counts.find_one({'source_id':self.data['_id']}, limit=1, sort=[('count', pymongo.DESCENDING)])['count']
-        cardinality = db.counts.find({'source_id':self.data['_id']}).count()
-        doc_count = cardinality - db.counts.find({'source_id':self.data['_id'], 'count':0}).count()
         for data in db.counts.find({'source_id':self.data['_id']}):
             data['normalized'] = data['count'] / peak
-            data['tfidf'] = data['count'] * math.log(cardinality / doc_count, 10)
             del data['_id']
             db.transformed.insert(data)
-        self.data['status'] = TaskStatus.TRANSFORMED
-        self.save()
         
     def load(self):
-        self.data['status'] = TaskStatus.LOADING
-        self.save()
         for data in db.transformed.find({'source_id':self.data['_id']}):
             db.results.insert({
                 'source_id': self.data['_id']
                 , 'project_id': self.data['project_id']
                 , 'label': self.data['label']
                 , 'date': data['date']
-                , 'value': data['tfidf']
+                , 'value': data['normalized']
             })
-        self.data['status'] = TaskStatus.COMPLETE
-        self.save()
             
     @classmethod
     def create_form(cls):
